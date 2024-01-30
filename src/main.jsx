304472157b8900294ad76ab629df523545b60c85
Bhanu8505 <bhanupundir8505@gmail.com>
2024-01-30 13:48:52 +0530

    Added tailwind config

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+// Components import
+import App from './App.jsx';
+// CSS imports
+import './index.css';
+// Library imports
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {BrowserRouter} from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-
+  <BrowserRouter>
     <App />
+  </BrowserRouter>
+    
 
 );
